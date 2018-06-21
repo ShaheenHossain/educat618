bdfded916b66ddc823b3a469532bc6b0b4e5449f
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-06-22 04:10:29 +0600

    Update __manifest__.py

--- openeducat_fees/__manifest__.py
+++ openeducat_fees/__manifest__.py
@@ -1,34 +1,13 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#
-#    Tech-Receptives Solutions Pvt. Ltd.
-#    Copyright (C) 2009-TODAY Tech-Receptives(<http://www.techreceptives.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
-
 {
-    'name': 'OpenEduCat Fees',
+    'name': 'Eagle Education Fees',
     'version': '10.0.3.0.0',
     'license': 'LGPL-3',
     'category': 'Education',
     "sequence": 3,
     'summary': 'Manage Fees',
     'complexity': "easy",
-    'author': 'Tech Receptives',
-    'website': 'http://www.openeducat.org',
+    'author': 'Md. Shaheen Hossain',
+    'website': 'http://www.eagle-erp.com',
     'depends': ['openeducat_core', 'account_accountant'],
     'data': [
         'views/fees_terms_view.xml',
