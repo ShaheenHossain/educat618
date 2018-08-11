adc0e9f986c182dc345ff0ce595aaf514dc5bd4d
ShaheenHossain <rapidgrps@princegroup-bd.com>
2018-08-11 12:51:54 +0600

    Add files via upload

---
+++ eagle_sms/models/__init__.py
@@ -0,0 +1,5 @@
+import res_partner, sms_account, res_country, ir_actions_server, sms_number, sms_message, sms_gateway, sms_compose, sms_template,  ir_attachment
+import op_student
+import sms_gateway_me
+import sms_gateway_ssl
+import me
